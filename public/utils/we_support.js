2f7c6c6ba8740d8f49044f1d850551bb1bfaa535
Rony Langbord <rony.langbord@wework.com>
2018-07-18 16:59:27 +0300

    added component dynamically

--- public/utils/we_support.js
+++ public/utils/we_support.js
@@ -1,16 +1,35 @@
 $(document).ready(function() {
-    $('body').append
-    (`<div class="btn_container">
+  appendAccessabilityButton();
+  appendAccessabilityMenu();
+});
+
+function openAccessabilityMenu() {
+    document.getElementById('sidebar-container').style.width = "250px";
+}
+
+function appendAccessabilityMenu() {
+  $('body').append(
+    `<div id="sidebar-container" class="sidebar-container">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeAccessabilityMenu()">&times;</a>
+      <button class="icon_increase-font" onClick='increaseFont()'>Increase Font</button>
+      <button class="icon_decrease-font" onClick='decreaseFont()'>Decrease Font</button>
+      <button>Black and White</button>
+      <button>Increase Font</button>
+      <button>Increase Font</button>
+      <button>Increase Font</button>
+    </div>`);
+}
+
+function appendAccessabilityButton(){
+  $('body').append
+  (`<div class="btn_container">
         <button 
             class=float 
             onclick='openAccessabilityMenu()'>
         </button>
     </div>`);
-});
-
-function openAccessabilityMenu() {
-    document.getElementById("sidebar-container").style.width = "250px";
 }
+
 
 function closeAccessabilityMenu() {
     document.getElementById("sidebar-container").style.width = "0";
