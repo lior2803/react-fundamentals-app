0c25914928b3cdc3f30b5c7cd6d4c0bfcc4d756e
Rony Langbord <rony.langbord@wework.com>
2018-07-18 14:35:02 +0300

    Added side bar menu

---
+++ public/we_support.js
@@ -0,0 +1,15 @@
+/**
+ * Created by rlangbord on 18/07/2018.
+ */
+
+function openSideBar(){
+  console.log("opened")
+}
+
+function openNav() {
+  document.getElementById("sidebar-container").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("sidebar-container").style.width = "0";
+}
--- src/utils/we_support.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by rlangbord on 18/07/2018.
- */
-
-
